69495034fb63d8227bdea97bb0dc05032aa5a040
Lukas Baischer <lukas_baischer@gmx.at>
2025-11-23 21:55:36 +0100

    add pwm example with 8 PWMs

---
+++ SW/frdmmcxa153_pwm/Debug/drivers/fsl_reset.d
@@ -0,0 +1,104 @@
+drivers/fsl_reset.o drivers/fsl_reset.d: ../drivers/fsl_reset.c \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\source\mcux_config.h \
+ ../drivers/fsl_common.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/fsl_device_registers.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/MCXA153.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_ADC.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/MCXA153_COMMON.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\CMSIS/core_cm33.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\CMSIS/cmsis_version.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\CMSIS/cmsis_compiler.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\CMSIS/cmsis_gcc.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/system_MCXA153.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/MCXA153_features.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_AOI.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_CDOG.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_CMC.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_CRC.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_CTIMER.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_DEBUGMAILBOX.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_DMA.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_EIM.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_EQDC.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_ERM.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_FMC.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_FMU.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_FMUTEST.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_FREQME.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_GLIKEY.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_GPIO.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_I3C.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_INPUTMUX.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPCMP.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPI2C.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPSPI.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPTMR.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPUART.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_MRCC.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_OSTIMER.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_PORT.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_PWM.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_SCG.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_SPC.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_SYSCON.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_TRDC.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_USB.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_UTICK.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_VBAT.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_WAKETIMER.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_WUU.h \
+ C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_WWDT.h \
+ ../drivers/fsl_common_arm.h ../drivers/fsl_clock.h \
+ ../drivers/fsl_reset.h
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\source\mcux_config.h:
+../drivers/fsl_common.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/fsl_device_registers.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/MCXA153.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_ADC.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/MCXA153_COMMON.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\CMSIS/core_cm33.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\CMSIS/cmsis_version.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\CMSIS/cmsis_compiler.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\CMSIS/cmsis_gcc.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/system_MCXA153.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device/MCXA153_features.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_AOI.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_CDOG.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_CMC.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_CRC.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_CTIMER.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_DEBUGMAILBOX.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_DMA.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_EIM.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_EQDC.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_ERM.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_FMC.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_FMU.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_FMUTEST.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_FREQME.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_GLIKEY.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_GPIO.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_I3C.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_INPUTMUX.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPCMP.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPI2C.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPSPI.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPTMR.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_LPUART.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_MRCC.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_OSTIMER.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_PORT.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_PWM.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_SCG.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_SPC.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_SYSCON.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_TRDC.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_USB.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_UTICK.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_VBAT.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_WAKETIMER.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_WUU.h:
+C:\Users\lukas\Documents\MCUXpressoIDE_25.6.136\workspace\frdmmcxa153_pwm\device\periph/PERI_WWDT.h:
+../drivers/fsl_common_arm.h:
+../drivers/fsl_clock.h:
+../drivers/fsl_reset.h:
